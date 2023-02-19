4f20c6dc7eeaca802c47f9997c09c6da6ceeb5a6
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2023-02-19 16:13:43 +0400

    added change btns

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 import { initControl } from "./control.js";
+import { state } from "./state.js";
 
 const initPomodoro = () => {
   initControl();
+
+  state.activeTodo = {
+    id: "default",
+    pomodoro: 2,
+    title: "Помодоро",
+  };
 };
 
 initPomodoro();
--- js/timer.js
+++ js/timer.js
@@ -1,16 +1,18 @@
 import { alarm } from "./alarm.js";
+import { changeActiveBtn } from "./control.js";
 import { state } from "./state.js";
+import { addZero } from "./util.js";
 
 const minutesElems = document.querySelector(".time__minutes");
 const secondsElems = document.querySelector(".time__seconds");
 
-const showTime = (seconds) => {
-  minutesElems.textContent = Math.floor(seconds / 60);
-  secondsElems.textContent = seconds % 60;
+export const showTime = (seconds) => {
+  minutesElems.textContent = addZero(Math.floor(seconds / 60));
+  secondsElems.textContent = addZero(seconds % 60);
 };
 
 export const startTimer = () => {
-  state.timeLeft -= 1;
+  state.timeLeft -= 10;
 
   showTime(state.timeLeft);
 
@@ -20,5 +22,21 @@ export const startTimer = () => {
 
   if (state.timeLeft <= 0) {
     alarm();
+
+    if (state.status === "work") {
+      state.activeTodo.pomodoro += 1;
+
+      if (state.activeTodo.pomodoro % state.count) {
+        state.status = "break";
+      } else {
+        state.status = "relax";
+      }
+    } else {
+      state.status = "work";
+    }
+
+    state.timeLeft = state[state.status] * 60;
+    changeActiveBtn(state.status);
+    startTimer();
   }
 };
